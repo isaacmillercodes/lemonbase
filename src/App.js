d613b36c53e2c3dcb7c690e59e728015ee854b55
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2018-01-30 21:13:58 -0700

    created shell index page

--- src/App.js
+++ src/App.js
@@ -5,26 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
       <div class="row">
-        <div class="col s12 m6">
+        <div class="col s12 offset-m3 m6">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
-              <span class="card-title">Card Title</span>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <span class="card-title yellow-text lighten-1">Welcome to Lemonbase</span>
+              <p>Please enter the Bitcoin address you want to track below: </p>
             </div>
             <div class="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
+              <input placeholder="Enter a Bitcoin address" id="address" type="text" />
+              <button type="submit">Submit</button>
             </div>
           </div>
         </div>
